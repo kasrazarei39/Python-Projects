37b91d491aa7e375d2ecd69e1ebbc0f232cb5838
kasrazarei39 <kasrazarei39@gmail.com>
2020-12-26 13:54:04 +0330

    Print map

--- Tic Tac Toe/Tic-Tac-Toe.py
+++ Tic Tac Toe/Tic-Tac-Toe.py
@@ -1,9 +1,26 @@
-def insertBoard(letter, pos):
-    pass
+board = [' ' for x in range(10)]
+
+
+def insertLetter(letter, pos):
+    board[pos] = letter
 
 
 def spaceIsFree(pos):
-    pass
+    return board[pos] == ' '
+
+
+def printBoard(board):
+    print('   |   |')
+    print(' ' + board[1] + ' | ' + board[2] + ' | ' + board[3])
+    print('   |   |')
+    print('-----------')
+    print('   |   |')
+    print(' ' + board[4] + ' | ' + board[5] + ' | ' + board[6])
+    print('   |   |')
+    print('-----------')
+    print('   |   |')
+    print(' ' + board[7] + ' | ' + board[8] + ' | ' + board[9])
+    print('   |   |')
 
 
 def isWinner(bo, le):
