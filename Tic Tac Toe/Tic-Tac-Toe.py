5b8a83cce05281781307f2b186688904348f6240
kasrazarei39 <kasrazarei39@gmail.com>
2020-12-26 15:43:49 +0330

    Upgrade player move

--- Tic Tac Toe/Tic-Tac-Toe.py
+++ Tic Tac Toe/Tic-Tac-Toe.py
@@ -35,7 +35,21 @@ def isWinner(bo, le):
 
 
 def playerMove():
-    pass
+    run = True
+    while run:
+        move = input('Please select a position to place an \'X\' (1-9): ')
+        try:
+            move = int(move)
+            if move > 0 and move < 10:
+                if spaceIsFree(move):
+                    run = False
+                    insertLetter('X', move)
+                else:
+                    print('Sorry, this space is occupied!')
+            else:
+                print('Please type a number within the range!')
+        except:
+            print('Please type a number!')
 
 
 def selectRandom(li):
