12e1ab669463e8107284deada0cecd64237a7ef3
kasrazarei39 <kasrazarei39@gmail.com>
2020-12-26 13:21:31 +0330

    Create functions

--- Tic Tac Toe/Tic-Tac-Toe.py
+++ Tic Tac Toe/Tic-Tac-Toe.py
@@ -1,6 +1,34 @@
-def main():
-    print("test")
+def insertBoard(letter, pos):
     pass
 
 
-main()
+def spaceIsFree(pos):
+    pass
+
+
+def isWinner(bo, le):
+    pass
+
+
+def playerMove():
+    pass
+
+
+def selectRandom(li):
+    pass
+
+
+def compMove():
+    pass
+
+
+def isBoardFull(board):
+    pass
+
+
+def printBoard():
+    pass
+
+
+def main():
+    pass
